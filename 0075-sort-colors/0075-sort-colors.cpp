b1cda7fcb434cf01f3d9a79ff7ad60c18721cc3c
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-10 03:09:14 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int l=0;
-        int m=0;
-        int h=nums.size()-1;
-        while(m<=h){
-            if(nums[m]==0){
-                swap(nums[l],nums[m]);
-                l++;
-                m++;
-            }else if(nums[m]==1){
-                m++;
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
             }else{
-                swap(nums[m],nums[h]);
-                h--;
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
     }
