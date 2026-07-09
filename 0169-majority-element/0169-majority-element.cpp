fafcd3a9ab3594437b14d333ce22abfad5cdc5fb
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-10 04:30:54 +0530

    Time: 7 ms (10.44%), Space: 28.2 MB (33.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -17,19 +17,27 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+//         map<int,int>mpp;
+//         for(int i =0;i<n;i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(auto it:mpp){
+//             if(it.second > n/2){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        map<int,int>mpp;
-        for(int i =0;i<n;i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second > n/2){
-                return it.first;
-            }
-        }
-        return -1;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
     }
 };
