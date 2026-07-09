b7e866aa564d5fa4d02069283e88d64d1a9a78d7
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-09 08:11:06 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.15%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,28 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,int>mpp;
+//         for(int i=0;i<n;i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(auto it:mpp){
+//             if(it.second==1){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>mpp;
+        int xorr=0;
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
+            xorr=xorr ^ nums[i];
         }
-        for(auto it:mpp){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return -1;
+        return xorr;
     }
 };
