b358c8a930590c591662b68a2d2e18c2f746b3f7
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-11 19:47:07 +0530

    Time: 2227 ms (5.03%), Space: 28.2 MB (67.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,20 @@
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         for(int i=0;i<nums.size();i++){
-//             int count=0;
-//             for(int j=0;j<nums.size();j++){
-//                 if(nums[i]==nums[j]){
-//                     count++;
-//                 }
-                
-//             }
-//             if(count > nums.size()/2){
-//                 return nums[i];
-//             }
-//         }
-//         return -1;
-//     }
-// };
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      for(int i=0;i<nums.size();i++){
+      int count=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+        }
+        if(count>nums.size()/2){
+            return nums[i];
+        }
+      }
+      return -1;
+    }
+};
 
 // class Solution {
 // public:
@@ -34,10 +33,3 @@
 //     }
 // };
 
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
-    }
-};
