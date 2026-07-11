5c27590260d1988a3324dbdc6907fa831afe7b8e
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-11 20:25:08 +0530

    Time: 0 ms (100%), Space: 28.2 MB (67.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -16,17 +16,47 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+//         map<int,int>mpp;
+//         for(int i=0;i<n;i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(auto it:mpp){
+//             if(it.second>n/2){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int cnt=0;
+        int el;
         int n=nums.size();
-        map<int,int>mpp;
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+            }else if(nums[i]==el){
+                cnt++;
+            }else{
+                cnt--;
+            }
         }
-        for(auto it:mpp){
-            if(it.second>n/2){
-                return it.first;
+        int cnt1=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el){
+                cnt1++;
+            }
+            if(cnt1>n/2){
+                return el;
             }
         }
         return -1;
