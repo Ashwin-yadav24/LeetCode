60047f6e10e84b70cf21a3b0b37eb7ee96509cc2
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-12 19:28:33 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.38%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cSum=0;
+        int mSum=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            cSum+=nums[i];
+            mSum=max(mSum,cSum);
+            if(cSum<0){
+                cSum=0;
+            }
+        }
+        return mSum;
+    }
+};
