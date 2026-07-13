239a24c304b95ce2eb249c12c7fe561781cb7f06
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-13 15:09:16 +0530

    Time: 352 ms (5.05%), Space: 21.9 MB (38.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        for(int i =0;i<=n;i++){
+        for(int i=0;i<=n;i++){
             bool found = false;
             for(int j=0;j<n;j++){
                 if(nums[j]==i){
