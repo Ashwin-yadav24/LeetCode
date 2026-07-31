9a412ec0a39817b78e08baed767853c872decbe5
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-31 10:03:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (8.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -15,7 +15,7 @@
 //             ans.push_back(nums1[i++]);
 //         }
 //         while(j<n){
-//             ans.push_back(nums2[j++]); 
+//             ans.push_back(nums2[j++]);
 //         }
 //         for(int i=0;i<m+n;i++){
 //             nums1[i]=ans[i];
@@ -47,3 +47,4 @@ public:
         }
     }
 };
+
