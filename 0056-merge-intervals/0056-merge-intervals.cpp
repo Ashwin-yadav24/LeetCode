1c395d4df84c32a43fd1919e82c086d6d379f07a
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-07-31 10:54:53 +0530

    Time: 7 ms (38.13%), Space: 23.7 MB (95.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-
-        vector<vector<int>> merged;
-        vector<int> prev = intervals[0];
-
-        for (int i = 1; i < intervals.size(); ++i) {
-            vector<int> interval = intervals[i];
-            if (interval[0] <= prev[1]) {
-                prev[1] = max(prev[1], interval[1]);
-            } else {
-                merged.push_back(prev);
-                prev = interval;
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            if(ans.empty() || arr[i][0]>ans.back()[1]){
+                ans.push_back(arr[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
             }
         }
-
-        merged.push_back(prev);
-        return merged;        
+        return ans;
     }
 };
