db10f738559bcd8b323ff5cae739292b6b661fa5
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-08-03 09:38:55 +0530

    Time: 3 ms (66.28%), Space: 15.2 MB (9.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -12,17 +12,34 @@
 //         return {};  
 //     }   
 // };
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         map<int,int>mp;
+//         for(int i=0;i<n;i++){
+//             int need=target-nums[i];
+//             if(mp.find(need) != mp.end()){
+//                 return {mp[need],i};
+//             }
+//             mp[nums[i]]=i;
+//         }
+//         return {};
+//     }   
+// };
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        map<int,int>mp;
+        map<int,int>mpp;
         for(int i=0;i<n;i++){
             int need=target-nums[i];
-            if(mp.find(need) != mp.end()){
-                return {mp[need],i};
+            if(mpp.find(need)!=mpp.end()){
+                return {mpp[need],i};
             }
-            mp[nums[i]]=i;
+            mpp[nums[i]]=i;
         }
         return {};
     }   
