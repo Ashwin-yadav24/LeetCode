0afa1e81adf73bf3496c0f4e1dc310e647f7c288
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-08-03 11:00:46 +0530

    Time: 0 ms (100%), Space: 128.3 MB (54.79%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -29,9 +29,10 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n=nums.size();
+        int pos=0;
+        int neg=1;
         vector<int>ans(n);
-        int pos=0,neg=1;
-        for(int x : nums){
+        for(int x:nums){
             if(x>0){
                 ans[pos]=x;
                 pos+=2;
@@ -40,7 +41,6 @@ public:
                 neg+=2;
             }
         }
-        
         return ans;
     }
 };
