cdb52d9c2c27ef912b238163f42b61ed0678e422
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-08-06 23:54:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (73.4%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-
-    int digitProduct(int x) {
-        int product = 1;
-
-        while (x > 0) {
-            product *= (x % 10);
-            x /= 10;
+    int digitProduct(int x){
+        int product=1;
+        while(x>0){
+            product*=x%10;
+            x/=10;
         }
-
         return product;
     }
+ 
 
     int smallestNumber(int n, int t) {
-        while (true) {
-            if (digitProduct(n) % t == 0)
-                return n;
+        while(true){
+        if (digitProduct(n)%t==0)
+            return n;
             n++;
         }
     }
