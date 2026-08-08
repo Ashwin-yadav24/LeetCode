17f6dd32b356d5b067b745d71b8a5268111ef2dd
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-08-08 21:11:17 +0530

    Time: 35 ms (35.41%), Space: 14.1 MB (84.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -12,19 +12,33 @@
 //         return {};
 //     }
 // };
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        map<int, int> mpp;
-        for (int i = 0; i < n; i++) {
-            int need = target - nums[i];
-            if (mpp.find(need) != mpp.end()) {
-                return {mpp[need], i};
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
             }
-            mpp[nums[i]]=i;
         }
         return {};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         map<int, int> mpp;
+//         for (int i = 0; i < n; i++) {
+//             int need = target - nums[i];
+//             if (mpp.find(need) != mpp.end()) {
+//                 return {mpp[need], i};
+//             }
+//             mpp[nums[i]]=i;
+//         }
+//         return {};
+//     }
+// };
