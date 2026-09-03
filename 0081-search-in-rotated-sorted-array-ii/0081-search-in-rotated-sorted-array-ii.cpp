2a394843e801afc5553285a43597c6d2d6a5564d
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-09-03 15:58:40 +0530

    Time: 0 ms (100%), Space: 17.9 MB (81.09%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
-            if (nums[mid] == target) {
-                return true;
-            }
-            
-            if (nums[mid] == nums[left]) {
-                left++;
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]== target) return true;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
                 continue;
             }
-            
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+            //left half
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+            }
+            //right half
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
                 }
             }
         }
-        
-        return false;        
+        return false;
     }
 };
