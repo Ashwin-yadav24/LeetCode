19e2daa1c867f0f3335b1b300da048a13a7b9044
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-09-04 12:45:10 +0530

    Time: 0 ms (100%), Space: 30.4 MB (76.6%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,19 +1,37 @@
+// class Solution {
+// public:
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             int maxi=nums[0];
+//             int mini=nums[i];
+//             for(int j=0;j<=i;j++){
+//                 maxi=max(maxi,nums[j]);
+//             }
+//             for(int j=i;j<n;j++){
+//                 mini=min(mini,nums[j]);
+//             }
+//             if(maxi - mini <= k){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
+        int maxi=nums[0];
+        int mini=nums[n-1];
         for(int i=0;i<n;i++){
-            int maxi=nums[0];
-            int mini=nums[i];
-            for(int j=0;j<=i;j++){
-                maxi=max(maxi,nums[j]);
-            }
-            for(int j=i;j<n;j++){
+            maxi=max(maxi,nums[i]);
+            mini=nums[i];
+            for(int j=i+1;j<n;j++){
                 mini=min(mini,nums[j]);
             }
-            if(maxi - mini <= k){
-                return i;
-            }
+            if(maxi-mini<=k) return i;
         }
         return -1;
     }
