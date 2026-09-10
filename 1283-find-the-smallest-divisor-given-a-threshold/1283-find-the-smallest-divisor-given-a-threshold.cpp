c66aaaef3cc80e10ac5ae3bf1344db91c52094c0
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-09-10 17:29:53 +0530

    Time: 17 ms (10.24%), Space: 26.2 MB (48.17%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,38 +1,25 @@
 class Solution {
+    private:
+        int func(vector<int>& nums,int div){
+            int sum=0;
+            int n=nums.size();
+            for(int i=0;i<n;i++){
+                sum=sum+ceil((double)(nums[i])/(double)(div));
+            }
+            return sum;
+        }
 public:
-    bool helperFunction(vector<int> &nums, int threshold, int d)
-    {
-        long long sum = 0;
-        int n = nums.size();
-
-        for(int i = 0; i < n; i++)
-        {
-            sum += ((nums[i] + d - 1) / d);
-        }
-
-        return (sum <= threshold);
-    }
-
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int left = 1;
-        int right = *max_element(nums.begin(), nums.end());
-        int ans = INT_MAX;
-
-        while(left <= right)
-        {
-            int mid = left + (right - left) / 2;
-
-            if(helperFunction(nums, threshold, mid))
-            {
-                ans = min(ans, mid);
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid + 1;
+        int low=1;
+        int high=*max_element(nums.begin(),nums.end());
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(func(nums,mid)<=threshold){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
-        return ans;
+        return low;
     }
 };
