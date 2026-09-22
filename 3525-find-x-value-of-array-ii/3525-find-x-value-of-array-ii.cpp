3e478a554f068f38a6ca2c378156091f4f50b68d
Ashwin-yadav24 <ashwinku.yadav2024@lpu.in>
2026-09-22 23:09:16 +0530

    Time: 587 ms (21.82%), Space: 388.8 MB (16.36%) - LeetHub

--- 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
@@ -1,130 +1,152 @@
 class Solution {
-    struct Info {
-        vector<int> ways;
-        int whole;
-
-        Info(int k) : ways(k, 0), whole(1) {}
-    };
-
-    struct SegmentTree {
-        int size;
-        int mod;
-        vector<Info> tree;
-
-        SegmentTree(vector<int>& nums, int k) {
-            mod = k;
-            size = 1;
-
-            while (size < nums.size()) {
-                size <<= 1;
-            }
-
-            tree.reserve(size * 2);
-
-            for (int i = 0; i < size * 2; i++) {
-                tree.emplace_back(k);
-            }
-
-            for (int i = 0; i < nums.size(); i++) {
-                int rem = nums[i] % k;
-
-                tree[size + i].ways[rem] = 1;
-                tree[size + i].whole = rem;
-            }
-
-            for (int i = size - 1; i > 0; i--) {
-                tree[i] = combine(
-                    tree[i << 1],
-                    tree[i << 1 | 1]
-                );
-            }
-        }
-
-        Info combine(const Info& left, const Info& right) {
-            Info merged(mod);
-
-            for (int r = 0; r < mod; r++) {
-                merged.ways[r] = left.ways[r];
-            }
-
-            for (int r = 0; r < mod; r++) {
-                if (right.ways[r] == 0) {
-                    continue;
-                }
-
-                int newRem = (left.whole * r) % mod;
-                merged.ways[newRem] += right.ways[r];
-            }
-
-            merged.whole = (left.whole * right.whole) % mod;
-
-            return merged;
-        }
-
-        void update(int index, int value) {
-            int pos = size + index;
-            int rem = value % mod;
-
-            fill(tree[pos].ways.begin(),
-                 tree[pos].ways.end(), 0);
-
-            tree[pos].ways[rem] = 1;
-            tree[pos].whole = rem;
-
-            pos >>= 1;
-
-            while (pos > 0) {
-                tree[pos] = combine(
-                    tree[pos << 1],
-                    tree[pos << 1 | 1]
-                );
-
-                pos >>= 1;
-            }
-        }
-
-        Info query(int left, int right) {
-            Info leftPart(mod);
-            Info rightPart(mod);
-
-            left += size;
-            right += size;
-
-            while (left < right) {
-                if (left & 1) {
-                    leftPart = combine(leftPart, tree[left]);
-                    left++;
-                }
-
-                if (right & 1) {
-                    right--;
-                    rightPart = combine(tree[right], rightPart);
-                }
-
-                left >>= 1;
-                right >>= 1;
-            }
-
-            return combine(leftPart, rightPart);
-        }
-    };
-
 public:
-    vector<int> resultArray(
-        vector<int>& nums,
-        int k,
-        vector<vector<int>>& queries
-    ) {
-        SegmentTree tree(nums, k);
-        vector<int> answer;
+    vector<int> resultArray(vector<int>& nums, int k, vector<vector<int>>& queries) {
+        int n = nums.size();
 
-        for (auto& q : queries) {
-            tree.update(q[0], q[1]);
-
-            Info result = tree.query(q[2], nums.size());
-            answer.push_back(result.ways[q[3]]);
+        int size = 1;
+        while (size < n) {
+            size <<= 1;
         }
 
-        return answer;
+        vector<long long> H(2 * size * k, 0);
+        vector<int> prod(2 * size, 1 % k);
+
+        for (int i = 0; i < n; i++) {
+            int v = nums[i] % k;
+            int nd = size + i;
+
+            H[nd * k + v] = 1;
+            prod[nd] = v;
+        }
+
+        for (int i = size - 1; i >= 1; i--) {
+            int lc = 2 * i;
+            int rc = lc + 1;
+            int lp = prod[lc];
+
+            int bi = i * k;
+            int bl = lc * k;
+            int br = rc * k;
+
+            for (int q = 0; q < k; q++) {
+                H[bi + q] = H[bl + q];
+            }
+
+            for (int q = 0; q < k; q++) {
+                long long c = H[br + q];
+
+                if (c) {
+                    H[bi + (lp * q) % k] += c;
+                }
+            }
+
+            prod[i] = lp * prod[rc] % k;
+        }
+
+        vector<int> ans;
+
+        for (auto& query : queries) {
+            int idx = query[0];
+            int val = query[1];
+            int start = query[2];
+            int x = query[3];
+
+            int nd = size + idx;
+            int base = nd * k;
+
+            for (int q = 0; q < k; q++) {
+                H[base + q] = 0;
+            }
+
+            int v = val % k;
+
+            H[base + v] = 1;
+            prod[nd] = v;
+
+            int cur = nd >> 1;
+
+            while (cur) {
+                int lc = 2 * cur;
+                int rc = lc + 1;
+                int lp = prod[lc];
+
+                int bi = cur * k;
+                int bl = lc * k;
+                int br = rc * k;
+
+                for (int q = 0; q < k; q++) {
+                    H[bi + q] = H[bl + q];
+                }
+
+                for (int q = 0; q < k; q++) {
+                    long long c = H[br + q];
+
+                    if (c) {
+                        H[bi + (lp * q) % k] += c;
+                    }
+                }
+
+                prod[cur] = lp * prod[rc] % k;
+                cur >>= 1;
+            }
+
+            int l = start + size;
+            int r = n + size;
+
+            vector<int> ln;
+            vector<int> rn;
+
+            while (l < r) {
+                if (l & 1) {
+                    ln.push_back(l);
+                    l++;
+                }
+
+                if (r & 1) {
+                    r--;
+                    rn.push_back(r);
+                }
+
+                l >>= 1;
+                r >>= 1;
+            }
+
+            reverse(rn.begin(), rn.end());
+
+            vector<long long> res(k, 0);
+            int p = 1 % k;
+
+            for (int node : ln) {
+                int b = node * k;
+
+                for (int q = 0; q < k; q++) {
+                    long long c = H[b + q];
+
+                    if (c) {
+                        res[(p * q) % k] += c;
+                    }
+                }
+
+                p = p * prod[node] % k;
+            }
+
+            for (int node : rn) {
+                int b = node * k;
+
+                for (int q = 0; q < k; q++) {
+                    long long c = H[b + q];
+
+                    if (c) {
+                        res[(p * q) % k] += c;
+                    }
+                }
+
+                p = p * prod[node] % k;
+            }
+
+            ans.push_back((int)res[x]);
+        }
+
+        return ans;
     }
 };
